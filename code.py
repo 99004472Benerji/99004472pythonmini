3f684acd0cdc88a736effa89e9a33da85956f486
99004472Benerji <lekkalasai.naidu@ltts.com>
2021-06-07 10:06:23 +0530

    update

--- code.py
+++ code.py
@@ -1,31 +1,37 @@
-
-import sys
 import openpyxl
+def Excel_info():
+    loc = "python.xlsx"
+    wb = openpyxl.load_workbook(loc)
 
-new = "python.xlsx"
-obj = openpyxl.load_workbook(new)
-sud = obj.sheetnames
-sh1 = obj['Marks']
-sh2 = obj['Hobbies']
-print("select the ps number ")
-num = int(input())
-row = sh1.max_row
-column = sh1.max_column
-A = 0
-B = 0
-if num:
-    for i in range(1, row + 1):
-        for j in range(1, column + 1):
-            if sh2.cell(i, j).value == num:
-                A = i + 1
-                B = j
-if A > 0 or B > 0:
-    print("data  found")
-else:
-    print("no data")
-    sys.exit()
-workbook = openpyxl.Workbook()
-sheet = workbook.active
-sheet.title = "Total_data"
-print("the data will print on new file ")
-workbook.save("new.xlsx")
+    sh = wb["Marks"]
+    for n in sh['A']:
+        print(n.value)
+
+    a = {}
+    file_obj = wb.active
+    columns = ['PS NO']
+
+    for sheet in wb.worksheets:
+        for i in range(2,sheet.max_row+1):
+            for j in range(2,sheet.max_column+1):
+                if sheet.cell(row=i,column=1).value not in a:
+                    a[sheet.cell(row=i,column=1).value] = []
+                if sheet.cell(row=1,column=j).value not in columns:
+                    columns.append(sheet.cell(row=1,column=j).value)
+                a[sheet.cell(row=i,column=1).value].append(sheet.cell(row=i,column=j).value)
+
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Total Info"
+
+    for i in range(len(columns)):
+        sheet.cell(row=1,column=i+1).value = columns[i]
+    keys = [int(input('Select a PS NO: '))]
+    for i in range(2,len(keys)+2):
+        sheet.cell(row = i,column=1).value=keys[i-2]
+        c=2
+    for j in a[keys[i-2]]:
+        sheet.cell(row=i,column=c).value=j
+        c+=1
+    workbook.save("Output.xlsx")
+Excel_info()
